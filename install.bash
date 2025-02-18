f32c9f9a11c2ceafe03056507674642f5683a79a
paulAlexSerban <paul.alex.serban@gmail.com>
2025-02-18 08:37:06 +0200

    feat: adjust install script

--- install.bash
+++ install.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# check if the entry.bash is already sourced in the .zshrc file and if not, add it
-grep -q 'source ~/development_scripts/entry.bash' ~/.zshrc || echo 'source ~/development_scripts/entry.bash' >> ~/.zshrc
+# check if the entry.bash is already sourced in the .zshrc file and if not, add it on the last line
+# grep -q 'source ~/development_scripts/entry.bash' ~/.zshrc || echo 'source ~/development_scripts/entry.bash' >> ~/.zshrc
+
+if grep -q 'source ~/development_scripts/entry.bash' ~/.zshrc; then
+  echo "entry.bash is already sourced in the .zshrc file"
+else
+  echo '' >>~/.zshrc
+  echo "source ~/development_scripts/entry.bash" >>~/.zshrc
+fi
 
 # check if the .env file is already created and if not, create it
 [ -f ~/development_scripts/.env ] || touch ~/development_scripts/.env
@@ -12,20 +19,19 @@ cp ~/development_scripts/.env.example ~/development_scripts/.env
 # prompt used for personal git username and adjust the variable in the .env file
 echo "Enter your personal git username:"
 read GITHUB_PERSONAL_USERNAME
-sed -i '' "s/GITHUB_PERSONAL_USERNAME=.*/GITHUB_PERSONAL_USERNAME=${GITHUB_PERSONAL_USERNAME}/" ~/development_scripts/.env
-
+sed -i '' "s/GITHUB_PERSONAL_USERNAME=.*/GITHUB_PERSONAL_USERNAME='${GITHUB_PERSONAL_USERNAME}'/" ~/development_scripts/.env
 
 # prompt used for personal git email and adjust the variable in the .env file
 echo "Enter your personal git email:"
 read GITHUB_PERSONAL_EMAIL
-sed -i '' "s/GITHUB_PERSONAL_EMAIL=.*/GITHUB_PERSONAL_EMAIL=${GITHUB_PERSONAL_EMAIL}/" ~/development_scripts/.env
+sed -i '' "s/GITHUB_PERSONAL_EMAIL=.*/GITHUB_PERSONAL_EMAIL='${GITHUB_PERSONAL_EMAIL}'/" ~/development_scripts/.env
 
 # prompt used for work git username and adjust the variable in the .env file
 echo "Enter your work git username:"
 read GITHUB_WORK_USERNAME
-sed -i '' "s/GITHUB_WORK_USERNAME=.*/GITHUB_WORK_USERNAME=${GITHUB_WORK_USERNAME}/" ~/development_scripts/.env
+sed -i '' "s/GITHUB_WORK_USERNAME=.*/GITHUB_WORK_USERNAME='${GITHUB_WORK_USERNAME}'/" ~/development_scripts/.env
 
 # prompt used for work git email and adjust the variable in the .env file
 echo "Enter your work git email:"
 read GITHUB_WORK_EMAIL
-sed -i '' "s/GITHUB_WORK_EMAIL=.*/GITHUB_WORK_EMAIL=${GITHUB_WORK_EMAIL}/" ~/development_scripts/.env
+sed -i '' "s/GITHUB_WORK_EMAIL=.*/GITHUB_WORK_EMAIL='${GITHUB_WORK_EMAIL}'/" ~/development_scripts/.env
