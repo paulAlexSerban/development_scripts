466b32535cfdd43d0351d3e2d8b98a4169acbe13
paulAlexSerban <paul.alex.serban@gmail.com>
2025-02-22 21:22:29 +0200

    feat: added sysinfo

--- entry.bash
+++ entry.bash
@@ -3,11 +3,12 @@
 source ~/development_scripts/.env
 source ~/development_scripts/tools/aws.bash
 source ~/development_scripts/tools/git.bash
+source ~/development_scripts/tools/k8s.bash
 source ~/development_scripts/tools/networking.bash
 source ~/development_scripts/tools/nvm.bash
+source ~/development_scripts/tools/sysinfo.bash
 source ~/development_scripts/tools/tab.bash
 source ~/development_scripts/tools/yarn.bash
-source ~/development_scripts/tools/k8s.bash
 
 echo "${BLUE}--- ~/development_scripts loaded ${NC}"
 sleep 3
---
+++ tools/sysinfo.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function get-sysinfo() {
+    echo "System Information" >/tmp/sysinfo.txt
+    echo "Date: $(date)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "Hostname: $(hostname)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "Kernel Version: $(uname -r)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "Uptime: $(uptime)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "File System: $(df -h)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "Network Configuration: $(ifconfig)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "Environment Variables: $(printenv)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+    echo "DNS Servers: $(cat /etc/resolv.conf | grep nameserver)" >>/tmp/sysinfo.txt
+    echo "--------------------------------------------------------------------------------" >>/tmp/sysinfo.txt
+
+    code /tmp/sysinfo.txt
+}
