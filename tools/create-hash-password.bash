36431cf9224cace2a52c06aca668b9bae7b302e8
Paul Serban <paul.alex.serban@gmail.com>
2025-06-16 14:57:17 +0300

    features/cli print (#7)
    * feat: add create hash password script (#1)
    
    * chore: bump version
    
    * feat: add docker check script (#3)
    
    * chore: bump version
    
    * fix: remove cd to specific script location (#5)
    
    * chore: bump fix
    
    * feat: add colors to messages

--- tools/create-hash-password.bash
+++ tools/create-hash-password.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-
 function create_hash_password() {
     local username="$1"
     local password="$2"
 
     if [[ -z "$username" || -z "$password" ]]; then
-        echo "Usage: create_hash_password <username> <password>"
+        echo "${RED}--- Usage: create_hash_password <username> <password> ${NC}"
         return 1
     fi
 
